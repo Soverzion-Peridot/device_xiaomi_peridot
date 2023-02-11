d7ec99bd8828d83633960973c5e3f08a5de8c041
Mathieu Chartier <mathieuc@google.com>
2023-02-11 10:17:52 +0000

    peridot: Speed profile services and wifi-service to reduce RAM and storage
    Signed-off-by: Drenzzz <realdrenzzz@gmail.com>

--- device.mk
+++ device.mk
@@ -522,6 +522,9 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/lineage/interfaces/power-libperfmgr \
     hardware/xiaomi
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.xiaomi-multihal \
