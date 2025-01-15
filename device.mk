5c5d04ddfcaaffc3ba4f68ae8708708c9388ef93
Guidix <guidixm@gmail.com>
2025-01-15 14:58:05 +0200

    peridot: Build FastCharge instead of lineage fastcharge
    - Add FastCharge to PRODUCT_PACKAGES.
    - Address FastCharge related SEpolicy rules.
    - Define sysfs_fastcharge as a domain.
    - Setup permission of fast charging node at boot.
    - Setup Fast Charging node using RRO overlay
    - Drop Lineage fastcharge related policies
    
    Change-Id: Ib66603768e6a3b24191f535fd5b7b4a54946804e
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: GuidixX <guidixm@gmail.com>
    Co-Authored-By: cyberknight777 <cyberknight755@gmail.com>
    Signed-off-by: Drenzzz <realdrenzzz@gmail.com>

--- device.mk
+++ device.mk
@@ -189,9 +189,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
-# Fastcharge
+# FastCharge
 PRODUCT_PACKAGES += \
-    vendor.lineage.fastcharge@1.0-service.xiaomi
+    FastCharge
 
 # Fingerprint
 TARGET_HAS_UDFPS := true
@@ -402,6 +402,7 @@ PRODUCT_PACKAGES += \
     SettingsProviderOverlayPeridotRedmi \
     SystemUIOverlayPeridot \
     TelephonyOverlayPeridot \
+    FastChargePeridot \
     WifiOverlayPeridot \
     WifiOverlayPeridotPOCO \
     WifiOverlayPeridotRedmi
