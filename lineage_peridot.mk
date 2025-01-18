amiami5/android_device_xiaomi_peridot

peridot: Updates from V816.0.12.0.UNPMIXM

lineage_peridot.mk
@@ -24,8 +24,8 @@ PRODUCT_BRAND := POCO
 PRODUCT_MODEL := 24069PC21G
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="peridot_global-user 14 UKQ1.240116.001 V816.0.9.0.UNPMIXM release-keys" \
-    BuildFingerprint=POCO/peridot_global/peridot:14/UKQ1.240116.001/V816.0.9.0.UNPMIXM:user/release-keys \
+    BuildDesc="peridot_global-user 14 UKQ1.240116.001 V816.0.12.0.UNPMIXM release-keys" \
+    BuildFingerprint=POCO/peridot_global/peridot:14/UKQ1.240116.001/V816.0.12.0.UNPMIXM:user/release-keys \
     DeviceName=peridot
     DeviceProduct=peridot_global \
     SystemName=peridot_global \

Soverzion-Peridot/device_xiaomi_peridot

lineage_peridot.mk
@@ -27,10 +27,10 @@ PRODUCT_SYSTEM_NAME := peridot_global
 PRODUCT_SYSTEM_DEVICE := peridot
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="peridot_global-user 14 UKQ1.240116.001 V816.0.10.0.UNPMIXM release-keys" \
+    PRIVATE_BUILD_DESC="peridot_global-user 14 UKQ1.240116.001 V816.0.12.0.UNPMIXM release-keys" \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
-BUILD_FINGERPRINT := POCO/peridot_global/peridot:14/UKQ1.240116.001/V816.0.10.0.UNPMIXM:user/release-keys
+BUILD_FINGERPRINT := POCO/peridot_global/peridot:14/UKQ1.240116.001/V816.0.12.0.UNPMIXM:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi